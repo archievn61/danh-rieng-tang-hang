e37277640e4b92ebf93451c766de7a01724d4ce5
archievn61 <88901229+archievn61@users.noreply.github.com>
2021-12-05 22:44:33 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
   text1: "He luu cậu!",
   text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
+  text3: "Tớ yêu cậu, cậu làm người yêu tớ nhaaa <3",
   text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text5: "Cậu đừng có mơ, lêu lêu",
+  text6: "Yêu ơi là yêu, tớ cũng yêu cậu <3",
+  text7: "Lí do cậu thích tớ đi :vvvv",
+  text8: "Gửi cho tớ nhé <3",
+  text9: "Vì cậu đáng yêuu mà Gấu béo của tớ",
+  text10: "Tớ biết mà ^^ Yêu cậu nhiều 3 triệu lầnn <3",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Tối tớ qua đón cậu đi chơi nhaa :v Xem được rồi thì nhắn tin lại cho tớ đi còn gìi <3",
   text12: "Okii lunn <3",
 };
 
